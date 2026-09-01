87f9005895e1e8e9106171b9a57bcff6d758f541
Emilie <emiliedumont29@gmail.com>
2026-09-01 10:07:06 +0200

    Découpe du template en partials

---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,14 @@
+<section class="ftco-section ftco-degree-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 ftco-animate">
+                <?php echo $content; ?>
+            </div>
+
+            <!-- .col-md-8 -->
+            <?php include '../app/views/templates/partials/_aside.php'; ?>
+            <!-- .col-md-4 -->
+
+        </div>
+    </div>
+</section> <!-- .section -->
