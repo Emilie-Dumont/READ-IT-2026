009542191ea032f2ffcab763490a9697d7cec2b0
Emilie <emiliedumont29@gmail.com>
2026-09-17 15:27:56 +0200

    Auteur reel d'un article + correction titre dynamique

--- www/app/controllers/postsController.php
+++ www/app/controllers/postsController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\PostsController;
 use \PDO;
 use \App\Models\PostsModel;
 use \App\Models\TagsModel;
+use \App\Models\AuthorsModel;
 
 function indexAction(PDO $connexion)
 {
@@ -19,11 +20,14 @@ function indexAction(PDO $connexion)
 function showAction(PDO $connexion, int $id)
 {
     include_once '../app/models/postsModel.php';
+    include_once '../app/models/authorsModel.php';
     include_once '../app/models/tagsModel.php';
     $post = PostsModel\findById($connexion, $id);
     $tags = TagsModel\findAllByPostId($connexion, $id);
+    $author = AuthorsModel\findOneById($connexion, $post['author_id']);
 
-    global $content;
+    global $content, $title;
+    $title = $post['title'];
     ob_start();
     include '../app/views/posts/show.php';
     $content = ob_get_clean();
---
+++ www/app/models/authorsModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\AuthorsModel;
+
+use \PDO;
+
+function findOneById(PDO $connexion, int $id)
+{
+    $sql = "SELECT *
+            FROM authors
+            WHERE id = :id;";
+
+    $rs = $connexion->prepare($sql);
+    $rs->bindValue(':id', $id, PDO::PARAM_INT);
+    $rs->execute();
+    return $rs->fetch(PDO::FETCH_ASSOC);
+}
