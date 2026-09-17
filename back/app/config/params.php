6fc89472c239664a4b906d2c0fa7a833d8750ac9
Emilie <emiliedumont29@gmail.com>
2026-09-17 14:40:21 +0200

    Backoffice : squelette MVC (dashboard) + integration du login

---
+++ back/app/config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+// Initialiser les zones dynamiques
+$title = "";
+$content = "Oups, il semble y avoir un problème.";
+
+// Paramètres de connexion
+define("DB_HOST", "localhost:3308");
+define("DB_NAME", "readit_2026");
+define("DB_USER", "root");
+define("DB_PWD", "");
---
+++ back/app/routers/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// ROUTE PAR DÉFAUT: DASHBOARD
+// PATTERN: /
+// URL: ?
+// CTRL: pagesController
+// ACTION: dashboard
+
+include_once '../app/controllers/pagesController.php';
+\App\Controllers\PagesController\dashboardAction($connexion);
